7e6c90d0d5776fd1ccc85d333d6577e0deccefc3
jdscdev <93043523+jdscdev@users.noreply.github.com>
2022-03-25 12:52:34 +0000

    Update RestModelController.java

--- demo/webgoat/src/main/java/com/mvcwebapp/webgoat/controllers/RestModelController.java
+++ demo/webgoat/src/main/java/com/mvcwebapp/webgoat/controllers/RestModelController.java
@@ -2,6 +2,7 @@ package com.mvcwebapp.webgoat.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.ws.rs.Produces;
 
@@ -18,86 +19,105 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-    
+
+@RequestMapping("/restxssmodel")
 @RestController
 public class RestModelController {
   // TP
-	@GetMapping(value = "/restxssmodel/tp/{tpParam}", produces = "text/html") // payload -> <img%20src=x%20onerror=alert(1)>
+	@GetMapping(value = "/tp/{tpParam}", produces = "text/html") // payload -> <img%20src=x%20onerror=alert(1)>
   public String methodTP(@PathVariable(value="tpParam") String tpParam, Model model) {
     model.addAttribute("tpParam", "TP - " + tpParam);
     return model.toString();
   }
-  @RequestMapping(value = "/restxssmodel/tp2/{tpParam2}")
+  @RequestMapping(value = "/tp2/{tpParam2}")
   @ResponseBody
   public String methodTP2(@PathVariable(value="tpParam2") String tpParam2) {
     Map<String, Object> map = new HashMap<>();
     map.put("tpParam", tpParam2);
 		return map.toString();
   }
-  @RequestMapping(value = "/restxssmodel/tp3/{tpParam3}")
+  @RequestMapping(value = "/tp3/{tpParam3}")
   public Object methodTP3(@PathVariable(value="tpParam3") String tpParam3) {
     ClassTest ct = new ClassTest();
     ct.setName(tpParam3);
 		return ct.getName();
   }
-	@GetMapping(value = "/restxssmodel/tp4/{tpParam4}")
+	@GetMapping(value = "/tp4/{tpParam4}")
   public ModelAndView methodTP4(@PathVariable(value="tpParam4") String tpParam4) {
     ModelAndView modelAndView = new ModelAndView("index");
     modelAndView.addObject("tpParam", "TP4 - " + tpParam4);
     return modelAndView;
   }
-  @RequestMapping(value = "/restxssmodel/tp5/", consumes = MediaType.APPLICATION_JSON_VALUE)
-  public String methodTP5(@RequestBody String tpParam5) {
+  @RequestMapping(value = "/tp5/{tpParam5}", produces = MediaType.APPLICATION_XML_VALUE)
+  public Object methodTP5(@PathVariable(value="tpParam5") String tpParam5) {
+    ClassTest ct = new ClassTest();
+		ct.setName(tpParam5);
 		return tpParam5;
   }
-
-	@GetMapping(value = "/restxssmodel/tp6/{tpParam6}", produces = "text/html")
+	@GetMapping(value = "/tp6/{tpParam6}", produces = "text/html")
 	@ResponseBody
 	public Object methodTP6(@PathVariable(value="tpParam6") String tpParam6) {
 		return ResponseEntity.status(HttpStatus.OK).body(tpParam6);
 	}
+	@RequestMapping(value = "/tp7/{tpParam7}")
+	@ResponseBody
+	public ResponseEntity<String> methodTP7(@PathVariable(value="tpParam7") String tpParam7) {
+    return ResponseEntity.ok(tpParam7);
+	}
+	@RequestMapping(value = "/tp8/{tpParam8}")
+	@ResponseBody
+	public ResponseEntity<String> methodTP8(@PathVariable(value="tpParam8") String tpParam8) {
+    return new ResponseEntity<>(tpParam8, null, HttpStatus.OK);
+	}
 
   // TNs XSS
-  @RequestMapping(value = "/restxssmodel/tn/{tnParam}")
+  @RequestMapping(value = "/tn/{tnParam}")
   public Model methodTN(@PathVariable(value="tnParam") String tnParam, Model model) {
     Map<String, String> map = new HashMap<>();
     map.put("tnParam", tnParam);
     model.mergeAttributes(map);
 		return model;
   }
-  @RequestMapping(value = "/restxssmodel/tn2/")
+  @RequestMapping(value = "/tn2/")
   public Model methodTN2(@RequestBody Model model) {
 		return model;
   }
-
-  @GetMapping(value = "/restxssmodel/tn3/{tnParam3}", produces = "text/html")
+  @GetMapping(value = "/tn3/{tnParam3}", produces = "text/html")
 	@ResponseBody
 	public Object methodTN3(@PathVariable(value="tnParam3") String tnParam3) {
 		ClassTest ct = new ClassTest();
 		ct.setName(tnParam3);
 		return ct;
 	}
-	
 	@Produces(MediaType.TEXT_HTML_VALUE)
-	@GetMapping(value = "/restxssmodel/tn4/{tnParam4}")
+	@GetMapping(value = "/tn4/{tnParam4}")
 	@ResponseBody
 	public Object methodTN4(@PathVariable(value="tnParam4") String tnParam4) {
 		ClassTest ct = new ClassTest();
 		ct.setName(tnParam4);
 		return ct;
 	}
-  
-  @RequestMapping(value = "/restxssmodel/tn6/{tnParam6}")
+  @RequestMapping(value = "/tn6/{tnParam6}")
   @ResponseBody
   public Map<String, Object> methodTN6(@PathVariable(value="tnParam6") String tnParam6) {
     Map<String, Object> map = new HashMap<>();
     map.put("tpParam", tnParam6);
 		return map;
   }
-  @RequestMapping(value = "/restxssmodel/tn7/{tnParam7}", produces = "application/json")
+  @RequestMapping(value = "/tn7/{tnParam7}", produces = "application/json")
   public ClassTest methodTN7(@PathVariable(value="tnParam7") String tnParam7) {
     ClassTest map = new ClassTest();
     map.setName(tnParam7);
 		return map;
   }
+  @RequestMapping(value = "/tn8/{tnParam8}", produces = "application/json")
+	@ResponseBody
+	public ResponseEntity<String> methodTN8(@PathVariable(value="tnParam8") String tnParam8) {
+    return ResponseEntity.ok(tnParam8);
+	}
+	@RequestMapping(value = "/tn9/{tnParam9}")
+	@ResponseBody
+	public Optional<String> methodTP9(@PathVariable(value="tnParam9") Optional<String> tnParam9) {
+    return tnParam9;
+	}
 }
